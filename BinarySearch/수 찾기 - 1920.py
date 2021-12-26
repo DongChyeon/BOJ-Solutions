ea9bfd9188871b4ab561e88cf230368f2062f0eb
Donghyeon Kim <kimdonghyun90766@gmail.com>
2021-12-26 23:03:41 +0900

    Update 수 찾기 - 1920.py

--- BinarySearch/수 찾기 - 1920.py
+++ BinarySearch/수 찾기 - 1920.py
@@ -1,10 +1,24 @@
 n = int(input())
-numbers = list(map(int, input().split()))
-m = int(input())
-choose = list(map(int, input().split()))
+a = sorted([int(i) for i in input().split()])
 
-for x in choose:
-    if x in numbers:
+m = int(input())
+numbers = [int(i) for i in input().split()]
+
+# 이분 탐색 이용
+for num in numbers:
+    start, end = 0, len(a) - 1
+    is_exist = False
+    while start <= end:
+        mid = (start + end) // 2
+        if a[mid] == num:
+            is_exist = True
+            break
+        elif a[mid] > num:
+            end = mid - 1
+        else:
+            start = mid + 1
+    
+    if is_exist:
         print(1)
     else:
         print(0)
