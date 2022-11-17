d31cf0c08d62c1e2b77d46f48835f77f9b3e865a
Donghyeon Kim <kimdonghyun90766@gmail.com>
2022-11-17 21:34:39 +0900

    Update 숫자 카드 2 - 10816.py

--- BinarySearch/숫자 카드 2 - 10816.py
+++ BinarySearch/숫자 카드 2 - 10816.py
@@ -1,47 +1,35 @@
-import sys
-
-# 찾는 값의 가장 작은 인덱스를 반환함
-def binary_search(array, target):
-    start, end = 0, len(array) - 1
-
-    while start <= end:
+# 찾고자 하는 값 이상이 처음 나타나는 위치를 구함
+def lower_bound(array, target):
+    start, end = 0, len(array)
+    
+    while start < end:
         mid = (start + end) // 2
-        if array[mid] == target:
-            while mid > 0:
-                if array[mid - 1] == target:
-                    mid -= 1
-                else:
-                    break
-            return mid
-        elif array[mid] > target:
-            end = mid - 1
+        
+        if target <= array[mid]:
+            end = mid
         else:
             start = mid + 1
+        
+    return start
+
+# 찾고자 하는 값보다 큰 값이 처음 나타나는 위치를 구함
+def upper_bound(array, target):
+    start, end = 0, len(array)
     
-    return None
-
-n = int(sys.stdin.readline())
-cards = list(map(int, sys.stdin.readline().split()))
-m = int(sys.stdin.readline())
-my_cards = list(map(int, sys.stdin.readline().split()))
-cards.sort()
-
-card_dict = {}
-
-for card in my_cards:
-    if card not in card_dict:
-        idx = binary_search(cards, card)
-        # 몇 개의 같은 수가 있는지 알아냄
-        if idx is not None:
-            count = 0
-            for i in range(idx, len(cards)):
-                if cards[i] == card:
-                    count += 1
-                else:
-                    break
+    while start < end:
+        mid = (start + end) // 2
+        
+        if target < array[mid]:
+            end = mid
         else:
-            count = 0
-        # 딕셔너리에 저장
-        card_dict[card] = count
+            start = mid + 1
+            
+    return start
+    
+n = int(input())
+my_cards = sorted(list(map(int, input().split())))
+m = int(input())
+cards = list(map(int, input().split()))
 
-print(' '.join(str(card_dict[x]) for x in my_cards))
+for card in cards:
+    print(upper_bound(my_cards, card) - lower_bound(my_cards, card), end=' ') 
