f8f6aff1cc5d8ba93542579a5fb0eb3f106bbc07
Donghyeon Kim <kimdonghyun90766@gmail.com>
2023-02-09 22:35:15 +0900

    Create 숫자 카드 - 10815.py

---
+++ BinarySearch/숫자 카드 - 10815.py
@@ -0,0 +1,22 @@
+def binary_search(data, target):
+    start, end = 0, len(data)
+    
+    while start <= end:
+        mid = (start + end) // 2
+        
+        if data[mid] == target:
+            return 1
+        elif data[mid] < target:
+            start = mid + 1
+        else:
+            end = mid - 1
+        
+    return 0
+
+n = int(input())
+cards = sorted(list(map(int, input().split())))
+m = int(input())
+target_cards = list(map(int, input().split()))
+
+for card in target_cards:
+    print(binary_search(cards, card), end = ' ')
